a4ee6adfab830c5765de8e897667c87aca3e4512
vjetcong#4836 <hoanghcm188@gmail.com>
2021-09-15 18:37:52 +0700

    update

--- 3/6.cpp
+++ 3/6.cpp
@@ -14,9 +14,25 @@ void display(int arr[], int n)
                 if(i < n-1) cout<<" ";
         }
 }
+
+void sapxep1(int arr[], int n)
+{
+    for(int i = 0; i < n - 1; i++){
+        for(int j = i + 1; j < n; j++){
+            if(arr[i] > arr[j]){
+                int flag = arr[i];
+                arr[i] = arr[j];
+                arr[j] = flag;
+            }
+        }
+    }
+}
+
 void insertElement(int arr[], int n, int val)
 {
-	_____
+    arr[n - 1] = val;
+
+    sapxep1(arr, n);
 }
 
 int main()
@@ -25,8 +41,8 @@ int main()
 	int n = 0, val = 0;
 	cin >> n >> val;
 	input(arr, n);
-	insertElement(arr, n, val);
 	n = n + 1;
+	insertElement(arr, n, val);
 	display(arr, n);
 	return 0;
 }
