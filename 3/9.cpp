fb23471b1639609118842a8d30250b7eb08f8c97
vjetcong#4836 <hoanghcm188@gmail.com>
2021-09-15 20:25:59 +0700

    Update 9.cpp

---
+++ 3/9.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+void inputMatrix(int arr[][100], int m, int n)
+{
+    for(int i = 0; i < m; i++)
+      for(int j = 0; j < n; j++)
+         cin>>arr[i][j];
+
+}
+void displayMatrix(int arr[][100], int m, int n)
+{
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cout<<arr[i][j];
+            if(i < n - 1) cout<<" ";
+        }
+
+        if(i < m - 1) cout<<endl;
+    }
+
+
+}
+int main() {
+    int m, n;
+    int arr[100][100];
+    cin >> m >> n;
+    inputMatrix(arr, m, n);
+    displayMatrix(arr, m, n);
+    return 0;
+}
