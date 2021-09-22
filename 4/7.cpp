e549daacbc20b444812d349fff83c9b6cfab4af4
vjetcong#4836 <hoanghcm188@gmail.com>
2021-09-23 00:23:22 +0700

    Update 7.cpp

--- 4/7.cpp
+++ 4/7.cpp
@@ -1,32 +1,32 @@
-#include <iostream>
-using namespace std;
-void xoa(char a[1000],int n,int x){
-        for(int i=x+1;i<n;i++){
-            a[i-1]=a[i];
-        }
-        a[n-1]='\0';
+    #include <iostream>
+    using namespace std;
+    void xoa(char a[1000],int n,int x){
+            for(int i=x+1;i<n;i++){
+                a[i-1]=a[i];
+            }
+            a[n-1]='\0';
 
-}
-void iFormatStr(char a[1000]){
-    int j=0;
-    int n=0,i=0;
-    while(a[i]!='\0'){
-        n++;
-        i++;
     }
-     while(a[j]!='\0'){
-        if(a[j]==a[j+1]){
-            xoa(a,n,j);
-            j--;
+    void iFormatStr(char a[1000]){
+        int j=0;
+        int n=0,i=0;
+        while(a[i]!='\0'){
+            n++;
+            i++;
         }
-        j++;
+         while(a[j]!='\0'){
+            if(a[j]==a[j+1]){
+                xoa(a,n,j);
+                j--;
+            }
+            j++;
+        }
+        cout<<a;
     }
-    cout<<a;
-}
-int main()
-{
-    char a[1000];
-    cin.getline(a,1000);
-    iFormatStr(a);
-    return 0;
-}
+    int main()
+    {
+        char a[1000];
+        cin.getline(a,1000);
+        iFormatStr(a);
+        return 0;
+    }
