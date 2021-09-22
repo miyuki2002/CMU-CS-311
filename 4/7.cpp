f3b47f668042b276ee00e71bc79f803abd636de8
vjetcong#4836 <hoanghcm188@gmail.com>
2021-09-23 00:22:29 +0700

    Update 7.cpp

--- 4/7.cpp
+++ 4/7.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 using namespace std;
-______ iFormatStr(______)
-{
-	______
+void xoa(char a[1000],int n,int x){
+        for(int i=x+1;i<n;i++){
+            a[i-1]=a[i];
+        }
+        a[n-1]='\0';
+
+}
+void iFormatStr(char a[1000]){
+    int j=0;
+    int n=0,i=0;
+    while(a[i]!='\0'){
+        n++;
+        i++;
+    }
+     while(a[j]!='\0'){
+        if(a[j]==a[j+1]){
+            xoa(a,n,j);
+            j--;
+        }
+        j++;
+    }
+    cout<<a;
 }
 int main()
 {
-    char str[______];
-    cin.getline(str, ______);
-    iFormatStr(str);
-    cout << str;
+    char a[1000];
+    cin.getline(a,1000);
+    iFormatStr(a);
     return 0;
 }
